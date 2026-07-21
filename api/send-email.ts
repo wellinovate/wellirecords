1c5aff22f3786b8d0a8b99de00342353fede43ea
Bossjaychin <talk2jaycode2@gmail.com>
2026-07-21 02:18:32 +0100

    Fix ESM import path in send-email function (missing .js extension broke all email templates)

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
-import { TEMPLATES } from './_lib/templates';
+import { TEMPLATES } from './_lib/templates.js';
 
 const resend = new Resend(process.env.RESEND_API_KEY ?? '');
 
